7e4eb5ea7c2268538c134cecb297dd66e37e1454
Victoria Shanukova <vshanukova@gmail.com>
2021-11-10 17:35:40 +0000

    redid the challenge

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :lists do
-    resources :bookmarks, only: %i[index new create]
+  resources :lists, except: %i[edit update] do
+    resources :bookmarks, only: %i[new create]
   end
   resources :bookmarks, only: :destroy
 end
