57ac20de6c06010841729b9420b995877ebdb15a
Victoria Shanukova <vshanukova@gmail.com>
2021-11-11 18:37:35 +0000

    redid challenge

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,12 @@ require("@rails/activestorage").start()
 require("channels")
 
 import 'bootstrap';
+import { initUpdateNavbarOnScroll } from '../components/navbar';
+
+document.addEventListener('turbolinks:load', () => {
+  // Call your JS functions here
+  initUpdateNavbarOnScroll();
+});
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
